3b116e30d9e73e1d6669c32891059f59896eb507
changpengcheng <wdcpclover@qq.com>
2025-02-18 02:06:07 +0800

    FIRST

---
+++ Chapter01/test_module.py
@@ -0,0 +1,8 @@
+def test_function():
+    print("This is a test function.")
+
+if __name__ == "__main__":
+    print("This module is being run directly.")
+    test_function()
+else:
+    print("This module is being imported.")
